ddfea172f89774d4ed5c3ee9c567b4027fc79324
arvindkalra <kalarvind97@gmail.com>
2018-03-02 00:24:09 +0530

    DB Connection Created

--- database/connect.js
+++ database/connect.js
@@ -1,1 +1,16 @@
-const
+const mongodb = require('mongodb');
+const config = require('../config.json');
+const db = config.database;
+const url = 'mongodb://'+db.username+':'+db.password+'@'+db.hostname+':'+db.port+'/'+db.dbname;
+
+module.exports = {
+    connect : (callback) => {
+        let self = this;
+        mongodb.connect(url, (err, database) => {
+           if(err) throw err;
+           console.log("Connected to Database = " + db.dbname);
+           self.obj = database;
+           callback();
+        });
+    }
+};
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 4000 || process.env.PORT;
-
+const db_connect = require('./database/connect');
 
 
 app.listen(PORT, () => {
-    console.log("Serving on http://localhost:" + PORT);
+    db_connect.connect(() => {
+        console.log("Serving on http://localhost:" + PORT);
+    });
 });
